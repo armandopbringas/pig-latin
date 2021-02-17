15ce359c690e70ce1284a77e0273e0f50fcf4899
armanopbringas <bringas.armandop@gmail.com>
2021-02-17 17:24:48 -0600

    adding gitignoer

--- pig-latin.js
+++ pig-latin.js
@@ -6,25 +6,38 @@
 function pigLatin(word) {
 
   const vowels = ['a', 'e', 'i', 'o', 'u'];
-  let isVowel = 0;
+  // let isVowel = 0;
 
-  if ( vowels.includes( word[0] ) ) {
-    return word + 'way';
-  } else {
-    for ( let consonant of word ) {
-      if ( vowels.includes(consonant) ) {
-        isVowel = word.indexOf(consonant);
-        // console.log(isVowel);
-        break;
-      }
-    }
-    const frstPart = word.slice(isVowel);
-    // console.log(frstPart);
-    const scnPart = word.slice(0, isVowel);
-    // console.log(scnPart);
-    return `${frstPart}${scnPart}ay`;
-  }
+  const [firstLetter] = word;
+  console.log(word);
+  const firstLetterIsVowel = vowels.some(vowel => vowel === firstLetter);
+  console.log(firstLetterIsVowel);
+
+  const lettersArray = [...word];
+  console.log(lettersArray);
+  const firstVowelIndex = lettersArray.findIndex(letter => vowels.contains(letter));
+  console.log(firstVowelIndex);
+
+  const frstPart = lettersArray.slice(1);
+  console.log(frstPart);
+
+  // if ( vowels.includes( word[0] ) ) {
+  //   return word + 'way';
+  // } else {
+  //   for ( let consonant of word ) {
+  //     if ( vowels.includes(consonant) ) {
+  //       isVowel = word.indexOf(consonant);
+  //       // console.log(isVowel);
+  //       break;
+  //     }
+  //   }
+  //   const frstPart = word.slice(isVowel);
+  //   // console.log(frstPart);
+  //   const scnPart = word.slice(0, isVowel);
+  //   // console.log(scnPart);
+  //   return `${frstPart}${scnPart}ay`;
+  // }
 
 }
 
-console.log(pigLatin('javascript'));
+console.log(pigLatin('angular'));
