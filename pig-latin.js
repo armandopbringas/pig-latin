d63786151939473c83782278aa3b304d8c1bdd49
armanopbringas <bringas.armandop@gmail.com>
2021-02-18 12:45:44 -0600

    adding code what returns the word in pig latin just if the word starts with vowel

--- pig-latin.js
+++ pig-latin.js
@@ -1,7 +1,8 @@
 // Pig Latin algorithm
 
 // - para palabras que comienzan con una vocal, se añade 'way' al final
-// - para palabras que comienzan con consonante, se mueven todas las consonantes antes de la primera vocal al final de la palabra y se añade 'ay'
+// - para palabras que comienzan con consonante, se mueven todas las consonantes 
+//   antes de la primera vocal al final de la palabra y se añade 'ay'
 
 function pigLatin(word) {
 
@@ -9,12 +10,19 @@ function pigLatin(word) {
 
   const [firstLetter] = word;
   const firstLetterIsVowel = vowels.some(vowel => vowel === firstLetter);
+  console.log(firstLetterIsVowel);
+
+  // Code what add 'way' at word ends
+  if(firstLetterIsVowel) {
+    return word + 'way';
+  }
 
   const lettersArray = [...word];
-  const firstVowelIndex = lettersArray.findIndex(letter => vowels.contains(letter));
-  
+  const firstVowelIndex = lettersArray.findIndex(letter => vowels.includes(letter));
   const frstPart = lettersArray.slice(1);
 
 }
+
+// pigLatin('angular');
 
 console.log(pigLatin('angular'));
