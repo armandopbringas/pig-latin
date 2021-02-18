c1d91873008c2785a2e59c12c8f76e6619ad32cf
armanopbringas <bringas.armandop@gmail.com>
2021-02-17 20:19:15 -0600

    removing commented code and console logs

--- pig-latin.js
+++ pig-latin.js
@@ -6,37 +6,14 @@
 function pigLatin(word) {
 
   const vowels = ['a', 'e', 'i', 'o', 'u'];
-  // let isVowel = 0;
 
   const [firstLetter] = word;
-  console.log(word);
   const firstLetterIsVowel = vowels.some(vowel => vowel === firstLetter);
-  console.log(firstLetterIsVowel);
 
   const lettersArray = [...word];
-  console.log(lettersArray);
   const firstVowelIndex = lettersArray.findIndex(letter => vowels.contains(letter));
-  console.log(firstVowelIndex);
-
+  
   const frstPart = lettersArray.slice(1);
-  console.log(frstPart);
-
-  // if ( vowels.includes( word[0] ) ) {
-  //   return word + 'way';
-  // } else {
-  //   for ( let consonant of word ) {
-  //     if ( vowels.includes(consonant) ) {
-  //       isVowel = word.indexOf(consonant);
-  //       // console.log(isVowel);
-  //       break;
-  //     }
-  //   }
-  //   const frstPart = word.slice(isVowel);
-  //   // console.log(frstPart);
-  //   const scnPart = word.slice(0, isVowel);
-  //   // console.log(scnPart);
-  //   return `${frstPart}${scnPart}ay`;
-  // }
 
 }
 
