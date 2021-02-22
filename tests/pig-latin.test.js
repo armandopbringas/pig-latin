44f29b259bf8f143ee0753d1fde81b446fd3c0d2
Diego Benjamín Aguilar Aguilar <diegoaguilaraguilar@gmail.com>
2021-02-22 17:47:28 -0600

    Tests structure

--- tests/pig-latin.test.js
+++ tests/pig-latin.test.js
@@ -1,9 +1,30 @@
-const firstLetterIsVowel = require('../pig-latin');
-const consonantLetters = require('../pig-latin');
+const { getConsonants, firstLetterIsVowel, pigLatin } = require('../pig-latin');
+
+const words = [ {word: "arquimides", assert: "yofo" }, "citlaltepetl", "tikua", "amlo", "hala madrid", "hola"];7
+
+
  
-test('the first word letter is vowel', () => {
-  expect(firstLetterIsVowel).toBe(true);
+
+describe("Unit tests", () => {
+  describe("First word letter checks", () => {
+
+    //expect(firstLetterIsVowel(/* test case variable * /)).toBe(true);
+    //expect(firstLetterIsVowel(/* test case variable 1 * /)).toBe(true);
+    //expect(firstLetterIsVowel(/* test case variable 2 * /)).toBe(true);
+    // const arrayThatHasSomeTestCases = [testCase1, testCase2, testCase3, testCase_n,];
+    // arrayThatHasSomeTestCases.forEach(testCase => expect(testCase).toEqual());
+  });
+
+  describe("Consonants check", () => {
+
+  });
 });
+
+describe("Big picture", () => {
+  const [ arquimides, pico, resto, malo, bestTeamEver, hola ] = words;
+  expect(pigLatin(arquimides)).toEqual("")
+})
+
 
 test('the consonantLetters array has vowles', () => {
   expect(consonantLetters).toContain('n');
