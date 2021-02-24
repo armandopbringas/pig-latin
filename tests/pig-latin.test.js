cbff0bb9b4af7e2a8e299a1219a0f44352dd6f85
Diego Benjamín Aguilar Aguilar <diegoaguilaraguilar@gmail.com>
2021-02-24 09:17:44 -0600

    Introduce describe blocks per method to be unit tested. Introduce the existance of a method brief description

--- tests/pig-latin.test.js
+++ tests/pig-latin.test.js
@@ -3,7 +3,11 @@ const words = [ { word: "arquimides", assert: true } ];
 
 describe('Unit tests', () => {
 
-  it('should be type string', () => {
+  describe('First #letterIsVowel', () => {
+    // It's fine to declare here a short description on
+    // why this method exists (as part of the overall solution)
+    // and a BRIEF on what's it's general behavior to come to a solution
+  })
     expect(() => firstLetterIsVowel()).toThrow(Error);
   });
 
