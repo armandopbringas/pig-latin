58fe31032f4aa7e00c7c43d67d769efa4b9935d2
armanopbringas <bringas.armandop@gmail.com>
2021-02-24 19:18:22 -0600

    adding test for check if a word starts with vowel is true or false

--- pig-latin.js
+++ pig-latin.js
@@ -5,13 +5,16 @@
 //   antes de la primera vocal al final de la palabra y se añade 'ay'
 
 const { vowels: VOWELS } = require('./constants');
-
+// const word = 'animal'
 const firstLetterIsVowel = word => {
   if (!word || typeof word !== 'string') throw new Error('Only strings allowed');
   const [firstLetter] = word;
-  return VOWELS.some(vowel => vowel === firstLetter);
+  // console.log(firstLetter);
+  const letter = VOWELS.some(vowel => vowel === firstLetter);
+  // console.log(letter);
+  return letter;
 }
-
+// console.log(firstLetterIsVowel(word));
 const getConsonants = word => {
   const wordLetters = [...word];
   const consonants = wordLetters.filter(wordLetter => !VOWELS.includes(wordLetter));
@@ -28,7 +31,7 @@ function pigLatin(word) {
 //   Step one: want to know if the first word letter is vowel
 //   Step two: the word starts with one or more consonants
 //   Step three: move consonant(s) at word ends
-//   return firstLetterIsVowel ? word + 'way' : word + 'ay';
+// return firstLetterIsVowel ? word + 'way' : word + 'ay';
 }
 
 module.exports = {
--- tests/pig-latin.test.js
+++ tests/pig-latin.test.js
@@ -1,10 +1,18 @@
+const { TestScheduler } = require('jest');
 const vowels = require('../constants');
 const { firstLetterIsVowel, getConsonants, pigLatin } = require('../pig-latin');
 
-describe('Unit tests', () => {
-
-  it('should be type string', () => {
+describe('testing firstLetterIsVowel function', () => {
+  test('should be type string', () => {
     expect(() => firstLetterIsVowel()).toThrow(Error);
   });
+  
+  test('should be start with vowel', () => {
+    expect(firstLetterIsVowel('animal')).toBeTruthy();
+  });
+  
+  test('should not start with vowel', () => {
+    expect(firstLetterIsVowel('true')).toBeFalsy();
+  });
+})
 
-}); 
