edf39e6cb0cd6ddbd00b0836700798708a99edc7
Diego Benjamín Aguilar Aguilar <diegoaguilaraguilar@gmail.com>
2021-02-25 09:54:34 -0600

    Include test cases to make Armando suffer

--- tests/pig-latin.test.js
+++ tests/pig-latin.test.js
@@ -2,13 +2,17 @@
 const vowels = require('../constants');
 const { firstLetterIsVowel, getConsonants, pigLatin } = require('../pig-latin');
 
-describe('testing firstLetterIsVowel function', () => {
+const { vowelStartingWords, nonVowelStartingWords } = require("./utils");
+
+describe('Testing firstLetterIsVowel function', () => {
   test('should be type string', () => {
     expect(() => firstLetterIsVowel()).toThrow(Error);
   });
   
-  test('should be start with vowel', () => {
-    expect(firstLetterIsVowel('animal')).toBeTruthy();
+  test('Correctly validate vowel starting words', () => {
+    vowelStartingWords.forEach(word => {
+      expect(firstLetterIsVowel(word)).toBeTruthy();
+    });
   });
   
   test('should not start with vowel', () => {
