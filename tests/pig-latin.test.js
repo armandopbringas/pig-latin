0781bcb871abc5b692b062a0744bca93db3218d6
armanopbringas <bringas.armandop@gmail.com>
2021-02-25 12:21:36 -0600

    adding test coverage command

--- tests/pig-latin.test.js
+++ tests/pig-latin.test.js
@@ -1,18 +1,15 @@
-
-const vowels = require('../constants');
 const { firstLetterIsVowel, getConsonants, pigLatin } = require('../pig-latin');
 
 describe('testing firstLetterIsVowel function', () => {
-  test('should be type string', () => {
+  it('should be type string', () => {
     expect(() => firstLetterIsVowel()).toThrow(Error);
   });
   
-  test('should be start with vowel', () => {
+  it('should be start with vowel', () => {
     expect(firstLetterIsVowel('animal')).toBeTruthy();
   });
   
-  test('should not start with vowel', () => {
+  it('should not start with vowel', () => {
     expect(firstLetterIsVowel('true')).toBeFalsy();
   });
-})
-
+});
