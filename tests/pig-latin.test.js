75b037ca5384d9d8f70fb41bb3aed3c004838b4a
Diego Benjamín Aguilar Aguilar <diegoaguilaraguilar@gmail.com>
2021-02-25 18:11:46 -0600

    Working tests

--- tests/pig-latin.test.js
+++ tests/pig-latin.test.js
@@ -1,28 +1,48 @@
-const { firstLetterIsVowel, getConsonants, pigLatin } = require('../pig-latin');
+const { firstLetterIsVowel, getConsonants, normalize, pigLatin } = require('../pig-latin');
 const { vowelStartingWords, nonVowelStartingWords } = require("./utils");
 
+const MIXED_CASE = "mIXeD_CaSe";
+
 describe('Testing input normalization', () => {
-  test('TBD 1', () => {
-    expect().toBeDefined();
+  
+  test('Should reject non string parameters', () => {
+    expect(() => normalize(1000)).toThrow();
   });
-  test('TBD 2', () => {
-    expect().toBeDefined();
+  
+  test('Should normalize strings into lower case characters', () => {
+    expect(normalize(MIXED_CASE)).toEqual("mixed_case");
   });
+
+  test("Should discard leading spaces", () => {
+    expect(normalize("    " + MIXED_CASE)).toEqual("mixed_case");
+  });
+
+  test("Should discard trailing spaces", () => {
+    expect(normalize(MIXED_CASE + "  ")).toEqual("mixed_case");
+  });
+
+  test("Should trim input", () => {
+    expect(normalize(" " + MIXED_CASE + "  ")).toEqual("mixed_case");
+  })
 })
 
 describe('Testing firstLetterIsVowel function', () => {
-  it('should be type string', () => {
+  it('Should handle non string parameters', () => {
     expect(() => firstLetterIsVowel()).toThrow(Error);
+    expect(() => firstLetterIsVowel(1000)).toThrow(Error);
+    expect(() => firstLetterIsVowel({})).toThrow(Error);
   });
   
   it('Correctly validate vowel starting words', () => {
-    vowelStartingWords.forEach( word => {
+    const normalizedWords = vowelStartingWords.map(normalize);
+    normalizedWords.forEach( word => {
       expect(firstLetterIsVowel(word)).toBeDefined(); 
       expect(firstLetterIsVowel(word)).toStrictEqual(true);
     });
   });
   
   it('should not start with vowel', () => {
+    const normalizedWords = vowelStartingWords.map(normalize);
     expect(firstLetterIsVowel('true')).toBeFalsy();
   });
 });
